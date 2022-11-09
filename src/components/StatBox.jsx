3f398cc2244403ab345f895ea3e5a5932d5ed63b
Gastón Mitoire <gastonmm@gmail.com>
2022-11-09 15:05:50 -0300

    fix: Reformas Dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -2,13 +2,47 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../theme";
 import ProgressCircle from "./ProgressCircle";
 
-const StatBox = ({ title, subtitle, icon, progress, increase }) => {
+const StatBox = ({ title, subtitle, icon, progress, dense }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
 
-  return (
-    <Box width="100%" m="0 30px">
-      <Box display="flex" justifyContent="space-between">
+  function truncate(string, limit) {
+    if (string.length <= limit) {
+      return string;
+    }
+
+    return string.slice(0, limit) + "...";
+  }
+
+  return dense ? (
+    <Box width="100%" sx={{ userSelect: "none" }}>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        p={0.5}
+      >
+        {icon}
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          sx={{ color: colors.grey[100] }}
+        >
+          {title}
+        </Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h5" sx={{ color: colors.greenAccent[100] }}>
+            {truncate(subtitle, 9)}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircle size="25" progress={progress} />
+        </Box>
+      </Box>
+    </Box>
+  ) : (
+    <Box width="100%" p="15px 30px">
+      <Box display="grid" gridTemplateColumns="repeat(2, minmax(0, 1fr))">
         <Box>
           {icon}
           <Typography
@@ -18,23 +52,15 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
           >
             {title}
           </Typography>
+          <Typography variant="h5" sx={{ color: colors.greenAccent[100] }}>
+            {subtitle}
+          </Typography>
         </Box>
-        <Box>
-          <ProgressCircle progress={progress} />
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <ProgressCircle size="75" progress={progress} />
         </Box>
       </Box>
-      <Box display="flex" justifyContent="space-between">
-        <Typography variant="h5" sx={{ color: colors.greenAccent[100] }}>
-          {subtitle}
-        </Typography>
-        <Typography
-          variant="h5"
-          fontStyle="italic"
-          sx={{ color: colors.greenAccent[600] }}
-        >
-          {increase}
-        </Typography>
-      </Box>
+      <Box display="flex" justifyContent="space-between"></Box>
     </Box>
   );
 };
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
@@ -6,94 +8,126 @@ import EmailIcon from "@mui/icons-material/Email";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CloseIcon from "@mui/icons-material/Close";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
 
+import Row1 from "./rows/Row1";
 import HotRecords from "../../components/HotRecords";
 import StatBox from "../../components/StatBox";
 
 const Dashboard = () => {
+  const [isDense, setIsDense] = useState(true);
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
+  const mainStatsData = [
+    {
+      title: "2,353",
+      subtitle: "Cobrados",
+      icon: (
+        <AccountBalanceWalletIcon
+          sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+        />
+      ),
+      progress: 0.35,
+    },
+    {
+      title: "2,353",
+      subtitle: "Urgentes",
+      icon: (
+        <LocalFireDepartmentIcon
+          sx={{ color: colors.redAccent[500], fontSize: "26px" }}
+        />
+      ),
+      progress: 0.53,
+    },
+    {
+      title: "2,353",
+      subtitle: "En Tratativa de Cobro",
+      icon: (
+        <AccountBalanceWalletIcon
+          sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+        />
+      ),
+      progress: 0.7,
+    },
+    {
+      title: "2,353",
+      subtitle: "Total",
+      icon: (
+        <AccountBalanceWalletIcon
+          sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+        />
+      ),
+      progress: 1,
+    },
+  ];
+
+  useEffect(() => {}, []);
 
   return (
     <Box m="0 20px 0 20px">
       <Header title="INICIO" subtitle="Bienvenido al tablero de estadísticas" />
 
       {/* GRID & CHARTS */}
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows={{ xs: "130px", lg: "170px" }}
-        gap="20px"
-      >
-        <HotRecords />
+      <Box display="grid" gap="5px">
         <Box
-          gridColumn={{
-            xs: "span 12",
-            sm: "span 4",
-            lg: "span 3",
-          }}
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          columnGap="20px"
+          onClick={
+            isDense
+              ? () => {
+                  setIsDense(!isDense);
+                }
+              : null
+          }
         >
-          <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
-            progress="0.75"
-            increase="+14%"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+          {mainStatsData.map(({ title, subtitle, icon, progress }, index) => (
+            <Box
+              key={index}
+              gridColumn="span 3"
+              backgroundColor={colors.primary[400]}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <StatBox
+                title={title}
+                subtitle={subtitle}
+                progress={progress}
+                dense={isDense}
+                icon={icon}
               />
-            }
-          />
+            </Box>
+          ))}
+          <Box
+            gridColumn="span 12"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ bgColor: "red" }}
+          >
+            <IconButton size="small" onClick={() => setIsDense(!isDense)}>
+              {isDense ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+            </IconButton>
+          </Box>
         </Box>
         <Box
-          gridColumn={{
-            xs: "span 12",
-            sm: "span 4",
-            lg: "span 3",
-          }}
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
+          display="grid"
+          gridTemplateColumns="repeat(12, minmax(0, 1fr))"
+          gridAutoRows={isDense ? "175px" : "115px"}
+          columnGap="20px"
         >
-          <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
-            progress="0.75"
-            increase="+14%"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        <Box
-          gridColumn={{
-            xs: "span 12",
-            sm: "span 4",
-            lg: "span 3",
-          }}
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="12,361"
-            subtitle="Emails Sent"
-            progress="0.75"
-            increase="+14%"
-            icon={
-              <EmailIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
+          <HotRecords />
+          <Box
+            gridColumn="span 9"
+            gridRow="span 2"
+            sx={{ bgcolor: "blue" }}
+          ></Box>
         </Box>
       </Box>
     </Box>
