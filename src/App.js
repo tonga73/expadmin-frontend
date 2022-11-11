c33e8c8fb33e63de27882a6cbbcb7d8f2643928a
Gastón Mitoire <gastonmm@gmail.com>
2022-11-11 17:42:37 -0300

    feat: container agregado a outlet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, Container, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import SidebarNavigation from "./scenes/global/Sidebar";
@@ -31,23 +31,28 @@ function App() {
           <SidebarNavigation />
           <main className="content">
             <Topbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/crear-expediente" element={<RecordCreateForm />} />
-              <Route path="/listado-expedientes" element={<RecordsList />} />
-              <Route path="/expedientes/:id" element={<Record />} />
+            <Container disableGutters>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route
+                  path="/crear-expediente"
+                  element={<RecordCreateForm />}
+                />
+                <Route path="/listado-expedientes" element={<RecordsList />} />
+                <Route path="/expedientes/:id" element={<Record />} />
 
-              <Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
-            </Routes>
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/geography" element={<Geography />} />
+                <Route path="/calendar" element={<Calendar />} />
+              </Routes>
+            </Container>
           </main>
         </div>
       </ThemeProvider>
